2d5e564fff6733c8098a4d5fb484aba57b8c3339
zivlakmilos <zivlakmilos@gmail.com>
2015-06-05 00:26:38 +0200

    takprog2 - d2_1

---
+++ programiranje/takprog2/d2_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, m;
+    scanf("%d %d", n, m);
+
+    unsigned int np[m];
+    int izn[m];
+    int tocak[n];
+    int tocakb[n];
+
+    for(i = 0; i < m; i++)
+        scnaf("%d %d", &np[i], &izn[i]);
+
+    return 0;
+}
